5a60132d5060f06982245af51fb7b63daf5ecf63
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-10 21:08:31 -0300

    configurando Eureka Client

--- hr-worker/src/main/java/com/spring/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/spring/hrworker/resources/WorkerResource.java
@@ -2,7 +2,6 @@ package com.spring.hrworker.resources;
 
 import com.spring.hrworker.entities.Worker;
 import com.spring.hrworker.repositories.WorkerRepository;
-import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
