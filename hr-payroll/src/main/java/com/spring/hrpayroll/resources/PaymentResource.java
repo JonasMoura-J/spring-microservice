4f96b85ccd5d456c38baa152aa25e5894a06a1db
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-11 00:47:24 -0300

    adicionando circuit braker para tolerancia de erros

--- hr-payroll/src/main/java/com/spring/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/spring/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.spring.hrpayroll.resources;
 
 import com.spring.hrpayroll.entities.Payment;
 import com.spring.hrpayroll.services.PaymentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +19,16 @@ public class PaymentResource {
         this.service = service;
     }
 
+    @CircuitBreaker(name = "hr-worker", fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
 
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days, Throwable t){
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
