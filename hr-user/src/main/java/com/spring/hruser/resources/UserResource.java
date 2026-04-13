7cae8c50854c0a38020f1e4fc3549a46cd7b817d
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-13 16:23:52 -0300

    Adicionando repository e controller de User

---
+++ hr-user/src/main/java/com/spring/hruser/resources/UserResource.java
@@ -0,0 +1,40 @@
+package com.spring.hruser.resources;
+
+import com.spring.hruser.entities.User;
+import com.spring.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RefreshScope
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    private final UserRepository repository;
+
+    public UserResource(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+
+        User user = repository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email) {
+
+        User user = repository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+}
