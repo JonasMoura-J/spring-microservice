9a63bf06a689d59205745998c499a086b9873376
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-13 16:04:35 -0300

    seed database

--- hr-user/src/main/java/com/spring/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/spring/hruser/HrUserApplication.java
@@ -1,13 +1,25 @@
 package com.spring.hruser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
 
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public HrUserApplication(BCryptPasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+//        System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+    }
 }
